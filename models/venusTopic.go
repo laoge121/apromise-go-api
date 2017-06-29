5ef56838c44302456c3980f97aaa9f36c1e08f04
yuhou <xupei0808@163.com>
2017-06-29 16:43:32 +0800

    删除

--- models/venusTopic.go
+++ models/venusTopic.go
@@ -46,14 +46,14 @@ func init() {
 	t := Topic{1, 2, "标题", "副标题", "标题图", "描述", "雨后", 1, 12, 23, 1}
 	TopicList[t.Title] = &t
 	orm.RegisterDriver("mysql", orm.DRMySQL)
-	orm.RegisterDataBase("default", "mysql", "test:test123@tcp(10.13.1.15:3306)/venus?charset=utf8", 30)
+	orm.RegisterDataBase("default", "mysql", "root:pwd@tcp(ip:3306)/dataBase?charset=utf8", 30)
 }
 
 func (t Topic) GetTopicById() {
 	o := orm.NewOrm()
 	o.Using("default")
 
-	r := o.Raw("SELECT title FROM t_venus_topic WHERE id =?", t.Id)
+	r := o.Raw("SELECT title FROM table WHERE id =?", t.Id)
 	var topic Topic
 	errs := r.QueryRow(&topic)
 	if errs != nil {
@@ -61,7 +61,7 @@ func (t Topic) GetTopicById() {
 	}
 	fmt.Println(topic)
 
-	r = o.Raw("update t_venus_topic set title=? where id =?", t.Title, t.Id)
+	r = o.Raw("update table set title=? where id =?", t.Title, t.Id)
 	results, err := r.Exec()
 	if err != nil {
 		fmt.Println(">>>不ok", err)
